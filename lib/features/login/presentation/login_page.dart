e08ffc9ef09d7cd51f98ea7e30da6d8bc8d016ca
gedeokta28 <40822018+gedeokta28@users.noreply.github.com>
2022-12-17 15:39:26 +0800

    Adjustment Login Page

--- lib/features/login/presentation/login_page.dart
+++ lib/features/login/presentation/login_page.dart
@@ -5,6 +5,7 @@ import 'package:provider/provider.dart';
 import '../../../../core/presentation/pages/main_page/main_page.dart';
 import '../../../core/presentation/widgets/custom_app_bar.dart';
 import '../../../core/presentation/widgets/rounded_button.dart';
+import '../../../core/static/app_config.dart';
 import '../../../core/static/dimens.dart';
 import '../../../core/utility/helper.dart';
 import '../../../core/utility/injection.dart';
@@ -33,14 +34,15 @@ class LoginPage extends StatelessWidget {
           child: ListView(
             children: [
               AspectRatio(
-                aspectRatio: 3 / 1.8,
+                aspectRatio:
+                    (App(context).appWidth(4) / App(context).appHeight(1.4)),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Image.asset(
                       ASSETS_LOGIN,
-                      width: 300.0,
+                      width: App(context).appWidth(80),
                     ),
                   ],
                 ),
