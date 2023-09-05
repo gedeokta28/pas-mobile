aaaf83105be495a67ed12d8575bdaad28e646a1a
gedeokta28 <40822018+gedeokta28@users.noreply.github.com>
2023-09-05 11:15:22 +0800

    Update Prod

--- lib/core/utility/app_settings.dart
+++ lib/core/utility/app_settings.dart
@@ -2,8 +2,8 @@ const String APP_NAME = 'PAS MOBILE';
 const String APP_DESCRIPTON_TITLE = 'PT. PARAMA ASIA SEJAHTERA';
 const String APP_DESCRIPTON_SUBTITLE = 'DISTRIBUTOR ALAT TEKNIK';
 
-const String BASE_URL =
-    'https://mediabalitech.com/mediabalitech.com/admin-pas/public/';
+const String BASE_URL = 'https://pasmobile-admin.com/';
+// 'https://mediabalitech.com/mediabalitech.com/admin-pas/public/';
 
 List<String> cities = [
   'New York',
